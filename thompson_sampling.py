24c9bf0a529a88cdf53584a2582cd1fdd48fbc04
chakradharreddygangadasu <57366432+chakradharreddygangadasu@users.noreply.github.com>
2020-09-08 12:58:00 -0500

    Add files via upload

---
+++ thompson_sampling.py
@@ -0,0 +1,49 @@
+# Thompson Sampling
+
+""" There is a company trying to promote their product through advertisements and came up with 10 possible
+advertisements. One advertise should be selected among the available 10 videos so that it can attrat the maximum 
+customers. The attentions of the custromers can be captured from a sample of 10000 advertisements and the 
+" click through rate" of each add displayed. The add that got the highest attention can be seen by using 
+the Thompson Sampling algorithm.""" 
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Ads_CTR_Optimisation.csv')
+
+# Implementing Thompson Sampling
+import random
+N = 10000
+d = 10
+ads_selected = []
+numbers_of_rewards_1 = [0] * d
+numbers_of_rewards_0 = [0] * d
+total_reward = 0
+for n in range(0, N):
+    ad = 0
+    max_random = 0
+    for i in range(0, d):
+        random_beta = random.betavariate(numbers_of_rewards_1[i] + 1, numbers_of_rewards_0[i] + 1)
+        if random_beta > max_random:
+            max_random = random_beta
+            ad = i
+    ads_selected.append(ad)
+    reward = dataset.values[n, ad]
+    if reward == 1:
+        numbers_of_rewards_1[ad] = numbers_of_rewards_1[ad] + 1
+    else:
+        numbers_of_rewards_0[ad] = numbers_of_rewards_0[ad] + 1
+    total_reward = total_reward + reward
+
+# Visualising the results - Histogram
+plt.hist(ads_selected)
+plt.title('Histogram of ads selections')
+plt.xlabel('Ads')
+plt.ylabel('Number of times each ad was selected')
+plt.show()
+
+"""From the histogram it is clear that the advertisement of index 4 has the highest attention from the customers
+so promoting the advertisements with that advertisement would give good results in promoting the product"""
